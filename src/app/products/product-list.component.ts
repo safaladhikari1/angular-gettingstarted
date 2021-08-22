a6fa232cece3bee73eeb5013bc5dc649f9c0413e
Safal Adhikari <safal.adhikari1@outlook.com>
2021-08-22 15:56:21 -0700

    defines IProduct interface for products Iproduct array

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
@@ -14,7 +15,7 @@ export class ProductListComponent {
     showImage: boolean = false;
     listFilter: string = 'cart';
 
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
